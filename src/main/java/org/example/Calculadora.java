cac6ec8b4e5daaf4ca1a0352342864d35501e1eb
Alisson <alisson.buffetti@gmail.com>
2024-08-09 22:28:43 -0300

    Aula 1 - Calculadora

--- src/main/java/org/example/Calculadora.java
+++ src/main/java/org/example/Calculadora.java
@@ -1,2 +1,24 @@
-package org.example;public class Calculadora {
+package org.example;
+
+public class Calculadora {
+    private int numeroA;
+    private int numeroB;
+
+    //Constructor
+    public Calculadora(int a, int b){
+        this.numeroA = a;
+        this.numeroB = b;
+    }
+
+    //Método
+    int somar(){
+        return this.numeroA + this.numeroB;
+    }
+
+    int subtrair(){
+        return this.numeroA - this.numeroB;
+
+    }
 }
+
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,2 +1,13 @@
-package org.example;public class main {
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Calculadora calc = new Calculadora(2,2);
+
+        System.out.println("Resultado = "+ calc.somar());
+
+
+    }
 }
